0a3bcaa6851749e8d99bf24dd846ce3c43937300
CShepartd <6ea3c7a3@10minutmail.pl>
2017-02-09 12:43:57 +0100

    Initial

---
+++ src/ASPNET_Core_Ajax_Example/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_Core_Ajax_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Ajax()
+        {
+            ViewData["Message"] = "Ajax Example";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ajax(string name1, string name2)
+        {
+            ViewData["Message"] = "Ajax Example";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
